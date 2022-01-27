f097a718fe25b4109d84db064301a00a5d6067b6
Christopher <cgw0011@mix.wvu.edu>
2022-01-27 00:56:12 -0500

    Added files for first navbar

---
+++ src/app/navigation/sidenavbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'yumly-sidenavbar',
+    templateUrl: 'sidenavbar.component.html',
+    styleUrls: ['sidenavbar.component.css']
+})
+export class SideNavBarComponent {
+
+}
